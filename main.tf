466637f651cc6830ae367f4af058b9a477ac5925
Darsh8790 <42856457+Darsh8790@users.noreply.github.com>
2022-09-17 17:22:13 +0530

    change14

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
   required_version = ">=0.12"
-  #backend "s3" {
-   # bucket         = "bhsworld-terraform-s3"
-   # key            = "terraform.tfstate"
-   # region         = "ap-south-1"
-   # dynamodb_table = "aws-locks"
-   # encrypt        = true
-  #}
+  backend "s3" {
+    bucket         = "bhsworld-terraform-s3"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
@@ -20,15 +20,15 @@ provider "aws" {
 
 
 # #Call the seed_module to build our ADO seed info
-module "bootstrap" {
-  source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "bhsworld-terraform-s3"
-  dynamo_db_table_name        = "aws-locks"
-  iam_user_name               = "GitHubActionsIamUser"
-  ado_iam_role_name           = "GitHubActionsIamRole"
-  aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
-  aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
-}
+#module "bootstrap" {
+  #source                      = "./modules/bootstrap"
+  #name_of_s3_bucket           = "bhsworld-terraform-s3"
+  #dynamo_db_table_name        = "aws-locks"
+  #iam_user_name               = "GitHubActionsIamUser"
+  #ado_iam_role_name           = "GitHubActionsIamRole"
+  #aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
+  #aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
+#}
 /*
 # Build the VPC
 resource "aws_vpc" "vpc" {
