41aa6e5b6a7d43ed5502dd18904db3dc1963e47a
Darsh8790 <42856457+Darsh8790@users.noreply.github.com>
2022-09-18 22:07:50 +0530

    update2

--- main.tf
+++ main.tf
@@ -1,3 +1,61 @@
+# Defining Region
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+# Defining CIDR Block for VPC
+variable "vpc_cidr" {
+  default = "30.0.0.0/16"
+}
+
+# Defining CIDR Block for Subnet
+variable "subnet_cidr" {
+  default = "30.0.1.0/24"
+}
+
+# Defining CIDR Block for 2d Subnet
+variable "subnet1_cidr" {
+  default = "30.0.2.0/24"
+}
+
+
+# Defining Public Key
+variable "public_key" {
+  default = "tests.pub"
+}
+
+# Defining Private Key
+variable "private_key" {
+  default = "tests.pem"
+}
+
+# Definign Key Name for connection
+variable "key_name" {
+  default = "tests"
+  description = "Desired name of AWS key pair"
+}
+
+
+
+# Defining AMI
+variable "ami" {
+ # default = "ami-01c1009f059c6e694"
+ # default = "ami-07050b42151ef470c"
+  default = "ami-0745276ff99a9e05d"
+}
+
+# Defining Instace Type
+variable "instancetype" {
+  default = "t3.medium"
+}
+
+# Defining Master count
+variable "master_count" {
+  default = 1
+}
+[ec2-user@ip-10-10-1-181 ~]$
+[ec2-user@ip-10-10-1-181 ~]$
+[ec2-user@ip-10-10-1-181 ~]$  cat main.tf
 # Configure and downloading plugins for aws
 provider "aws" {
   region     = "${var.aws_region}"
@@ -13,7 +71,7 @@ resource "aws_vpc" "demovpc" {
   }
 }
 
-# Creating Internet Gateway 
+# Creating Internet Gateway
 resource "aws_internet_gateway" "demogateway" {
   vpc_id = "${aws_vpc.demovpc.id}"
 }
@@ -25,24 +83,24 @@ resource "aws_route" "internet_access" {
   gateway_id             = "${aws_internet_gateway.demogateway.id}"
 }
 
-# Creating 1st subnet 
+# Creating 1st subnet
 resource "aws_subnet" "demosubnet" {
   vpc_id                  = "${aws_vpc.demovpc.id}"
   cidr_block             = "${var.subnet_cidr}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "Demo subnet"
   }
 }
 
-# Creating 2nd subnet 
+# Creating 2nd subnet
 resource "aws_subnet" "demosubnet1" {
   vpc_id                  = "${aws_vpc.demovpc.id}"
   cidr_block             = "${var.subnet1_cidr}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
 
   tags = {
     Name = "Demo subnet 1"
@@ -80,37 +138,6 @@ resource "aws_security_group" "demosg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Splunk default port
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  # Replication Port
-  ingress {
-    from_port   = 8089
-    to_port     = 8089
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Management Port
-  ingress {
-    from_port   = 4598
-    to_port     = 4598
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Ingestion Port
-  ingress {
-    from_port   = 9997
-    to_port     = 9997
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   # Outbound Rules
   # Internet access to anywhere
@@ -123,76 +150,64 @@ resource "aws_security_group" "demosg" {
 }
 
 # Creating key pair
-resource "aws_key_pair" "demokey" {
-  key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key)}"
+data "aws_key_pair" "bhs-nginx-key" {
+  key_name   = "bhs-nginx-key"
+  include_public_key = true
 }
-  
+
 # Creating EC2 Instance
 resource "aws_instance" "demoinstance" {
 
-  # AMI based on region 
-  ami = "${lookup(var.ami, var.aws_region)}"
+  # AMI based on region
+  ami = "${var.ami}"
 
-  # Launching instance into subnet 
+  # Launching instance into subnet
   subnet_id = "${aws_subnet.demosubnet.id}"
 
-  # Instance type 
+  # Instance type
   instance_type = "${var.instancetype}"
-  
+
   # Count of instance
   count= "${var.master_count}"
-  
+
   # SSH key that we have generated above for connection
-  key_name = "${aws_key_pair.demokey.id}"
+  key_name = "${data.aws_key_pair.bhs-nginx-key.key_name}"
 
   # Attaching security group to our instance
   vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
 
-  # Attaching Tag to Instance 
+  # Attaching Tag to Instance
   tags = {
     Name = "Search-Head-${count.index + 1}"
   }
-  
-  # Root Block Storage
-  root_block_device {
-    volume_size = "10"
-    volume_type = "standard"
-  }
-  
-  #EBS Block Storage
-  ebs_block_device {
-    device_name = "/dev/sdb"
-    volume_size = "10"
-    volume_type = "standard"
-    delete_on_termination = false
-  }
-  
-  # SSH into instance 
+
+
+
+  # SSH into instance
   connection {
-    
+
     # Host name
     host = self.public_ip
     # The default username for our AMI
-    user = "ec2-user"
+    user = "ubuntu"
     # Private key for connection
     private_key = "${file(var.private_key)}"
     # Type of connection
     type = "ssh"
   }
-  
+
   # Installing splunk on newly created instance
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo chkconfig docker on",
-      "sudo yum install -y git",
-      "sudo chmod 666 /var/run/docker.sock",
-      "docker pull dhruvin30/dhsoniweb:v1",
-      "docker run -d -p 80:80 dhruvin30/dhsoniweb:latest"   
+    #  "A=`(sudo docker ps -a |awk '{print $1}'|grep -Ev CONTAINE)`",
+    #  "sudo docker rm $A",
+      "sudo apt-get update",
+      "sudo apt install docker.io -y",
+      "sudo snap install docker",
+      "sudo docker --version",
+      "sudo docker pull nginx",
+    #  "sudo docker run --name docker-nginx -p 80:80 -d nginx"
+       "sudo docker run --name docker-nginxibhs -p 80:80 -d -v ~/docker-nginx/bhs:/usr/share/nginx/html nginx"
   ]
  }
 }
