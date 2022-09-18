5bb03077a44fc72b5f4035836272bc69f8e99827
Darsh8790 <42856457+Darsh8790@users.noreply.github.com>
2022-09-18 12:31:03 +0530

    Changes in variables

--- vars.tf
+++ vars.tf
@@ -16,35 +16,34 @@ variable "key_name" {
 
 # Defining Region
 variable "aws_region" {
-  default = "us-east-1"
+  default = "ap-south-1"
 }
 
 # Defining CIDR Block for VPC
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  default = "30.0.0.0/16"
 }
 
 # Defining CIDR Block for Subnet
 variable "subnet_cidr" {
-  default = "10.0.1.0/24"
+  default = "30.0.1.0/24"
 }
 
 # Defining CIDR Block for 2d Subnet
 variable "subnet1_cidr" {
-  default = "10.0.2.0/24"
+  default = "30.0.2.0/24"
 }
 
 # Defining AMI
 variable "ami" {
   default = {
-    eu-west-1 = "ami-0ea3405d2d2522162"
-    us-east-1 = "ami-09d95fab7fff3776c"
+    ap-south-1 = "ami-01c1009f059c6e694"
   }
 }
 
 # Defining Instace Type
 variable "instancetype" {
-  default = "t2.medium"
+  default = "t3.medium"
 }
 
 # Defining Master count 
