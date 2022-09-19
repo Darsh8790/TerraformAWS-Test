e58a5b3cd373042d324a9844b95fa0740be8c221
Darsh8790 <42856457+Darsh8790@users.noreply.github.com>
2022-09-19 07:00:27 +0530

    change26

--- main.tf
+++ main.tf
@@ -27,12 +27,14 @@ module "bootstrap" {
 
 
 
-/*
+
 # Configure and downloading plugins for aws
 provider "aws" {
   region     = "${var.aws_region}"
 }
 
+  
+/*
 # Creating VPC
 resource "aws_vpc" "demovpc" {
   cidr_block       = "${var.vpc_cidr}"
@@ -183,6 +185,4 @@ resource "aws_instance" "demoinstance" {
   ]
  }
 }
-
-
 */
